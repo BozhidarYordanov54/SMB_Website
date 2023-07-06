8503747e05a8362acccb5a3c31b6be66c0177307
BozhidarYordanov54 <smvh@abv.bg>
2023-07-06 15:37:31 +0300

    Update
    Removed calendar and added animation to main page

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-      $(".animate-scroll").each(function() {
-        var position = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-  
-        if (scroll + windowHeight > position) {
-          $(this).addClass("animate");
-        }
-      });
+  $(window).scroll(function() {
+    $(".animate-scroll").each(function() {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+
+      if (scroll + windowHeight > position) {
+        $(this).addClass("animate");
+      }
     });
   });
-  
+});
