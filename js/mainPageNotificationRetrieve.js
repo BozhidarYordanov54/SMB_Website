65f2dea4cb8a8b51afdabaf6ae9f2b03d730fa5c
BozhidarYordanov54 <smvh@abv.bg>
2023-08-14 17:59:55 +0300

    Update mainPageNotificationRetrieve.js

--- js/mainPageNotificationRetrieve.js
+++ js/mainPageNotificationRetrieve.js
@@ -13,10 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
 function showNotification(message) {
     const notification = document.getElementById('notification');
     notification.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="none" d="M0 0h24v24H0z"/>
-            <path d="M6 11l3 3 7-7"></path>
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512">
+    <!--! Font Awesome Pro 6.4.2 
+    by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.
+     -->
+    <path fill="white" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 
+    0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
         <div class="notification-message">${message}</div>
     `;
     notification.style.backgroundColor = 'green'; // Change this to the desired background color
