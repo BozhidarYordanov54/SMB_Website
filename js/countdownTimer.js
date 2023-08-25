48a6e0bf059885ba80c492a61cabcaccc11763a4
BozhidarYordanov54 <smvh@abv.bg>
2023-08-25 18:23:31 +0300

    Updated countdownTimer to EET

--- js/countdownTimer.js
+++ js/countdownTimer.js
@@ -1,58 +1,56 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Set the date and time for the next event
-    var eventDate = new Date("2023-08-25T14:45:00Z");
+    // Set the dates for form open and close
+    var formOpenDate = new Date("2023-11-23T15:10:00+03:00"); // EEST (UTC+3)
+    var formCloseDate = new Date("2023-12-7T23:59:00+03:00"); // EEST (UTC+3)
 
     // Update the countdown every second
     var countdownTimer = setInterval(function() {
-        // Get the current date and time
-        var now = new Date().getTime();
+        // Get the current date and time in EET
+        var now = new Date();
+        var nowEET = new Date(now.getTime() + (now.getTimezoneOffset() + 180) * 60000); // Adjust for EET (UTC+3)
 
-        // Calculate the time remaining
-        var timeRemaining = eventDate - now;
+        // Calculate the time remaining for form open and close
+        var timeUntilOpen = formOpenDate - nowEET;
+        var timeUntilClose = formCloseDate - nowEET;
 
-        // Check if the event has already passed
-        if (timeRemaining <= 0) {
+        if (timeUntilOpen > 0) {
+            // Countdown until form opens
+            displayCountdown("Формата ще бъде отворена след: ", timeUntilOpen);
+            disableFormSubmission();
+        } else if (timeUntilClose > 0) {
+            // Countdown while form is open
+            displayCountdown("Формата ще бъде затворена след: ", timeUntilClose);
+            enableFormSubmission();
+        } else {
+            // Form is closed
             clearInterval(countdownTimer);
-            document.getElementById("countdown").innerHTML = "Event has started!";
-                    disableFormSubmission();
-                    return;
-                }
+            document.getElementById("countdown").innerHTML = "Формата е затворена.";
+            disableFormSubmission();
+        }
+    }, 1000);
 
-                // Calculate days, hours, minutes, and seconds
-                var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    // Display the countdown with appropriate message
+    function displayCountdown(message, timeRemaining) {
+        var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-                // Display the countdown
-                document.getElementById("countdown").innerHTML = "Формата ще бъде отворена след: " + days + "д " + hours + "ч " + minutes + "м " + seconds + "с";
+        document.getElementById("countdown").innerHTML = message + days + "д " + hours + "ч " + minutes + "м " + seconds + "с";
+    }
 
-                // Enable/disable form submission based on time remaining
-                if (timeRemaining > 21 * 24 * 60 * 60 * 1000) {
-                    disableFormSubmission();
-                } 
-                else if(timeRemaining <= 7 * 24 * 60 * 60 * 1000){
-                    disableFormSubmission();
-                }
-                else {
-                    enableFormSubmission();
-                }
-            }, 1000);
+    // Disable form submission
+    function disableFormSubmission() {
+        var submitButton = document.getElementById("submit-button");
+        submitButton.disabled = true;
+        submitButton.style.background = "gray";
+        submitButton.style.cursor = "not-allowed";
+    }
 
-            // Disable form submission
-            function disableFormSubmission() {
-                var submitButton = document.getElementById("submit-button");
-                submitButton.disabled = true;
-                submitButton.style.background = "gray";
-                submitButton.style.cursor = "not-allowed";
-                submitButton.value = "До състезанието има повече от 3 седмици!";
-            }
-
-            // Enable form submission
-            function enableFormSubmission() {
-                var submitButton = document.getElementById("submit-button");
-                submitButton.disabled = false;
-                submitButton.value = "Записване";
-                document.getElementById("countdown").innerHTML = "Формата ще бъде затворена след: " + days + "д " + hours + "ч " + minutes + "м " + seconds + "с";
-            }
-        });
+    // Enable form submission
+    function enableFormSubmission() {
+        var submitButton = document.getElementById("submit-button");
+        submitButton.disabled = false;
+        submitButton.value = "Записване";
+    }
+});
